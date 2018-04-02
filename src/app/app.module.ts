65cc1e662ebe0e8ee3b5e39b70346da74b453545
codeRusty <sourabh.rustagi@hotmail.com>
2018-04-02 23:55:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { ProductlistComponent } from './Components/productlist/productlist.component';
+import { ProductdetailComponent } from './Components/productdetail/productdetail.component';
+import { CartpageComponent } from './Components/cartpage/cartpage.component';
+import { PaymentPageComponent } from './Components/payment-page/payment-page.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductlistComponent,
+    ProductdetailComponent,
+    CartpageComponent,
+    PaymentPageComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
